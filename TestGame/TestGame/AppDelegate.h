e153cc1a54b500af11c70df9d93d187ac115630e
Manny Tenorio <tenoriom54@yahoo.com>
2016-03-20 18:23:46 -0400

    start

---
+++ TestGame/TestGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGame
+//
+//  Created by Manuel Tenorio on 3/3/16.
+//  Copyright © 2016 Volcano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

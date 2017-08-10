73b600397fd0b5fa688d15e0c5844759146b03d7
Manny <tenoriom@lafayette.edu>
2017-08-10 01:00:24 -0500

    Initializing Sky Runner

---
+++ Sky Rescue/Sky Rescue/GameplayScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameplayScene.swift
+//  Sky Rescue
+//
+//  Created by Manuel Tenorio on 8/10/17.
+//  Copyright © 2017 Manuel Tenorio. All rights reserved.
+//
+
+import SpriteKit
+
+class GameplayScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+}// Class
